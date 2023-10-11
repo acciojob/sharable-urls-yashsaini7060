a4b0f8c9c9142e1d462b7a92cfe4774359a8f999
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-10-12 00:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// your code here
+document.getElementById("myForm").addEventListener("submit", 
+    function(event){
+      event.preventDefault()
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+      if(name!="" && year!=""){
+        document.getElementById("url").innerHTML = "https://localhost:8080/?name=" + name + "&year=" + year;
+      } else if(name!="" && year==""){
+        document.getElementById("url").innerHTML = "https://localhost:8080/?name=" + name;
+      } else if(name=="" && year!=""){
+        document.getElementById("url").innerHTML = "https://localhost:8080/?" + "year=" + year;
+      }
+      
+    }
+  );
